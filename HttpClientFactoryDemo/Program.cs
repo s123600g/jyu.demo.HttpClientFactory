13601ef7b95f11b62e168a4fda1ae187ba5479dd
JYUN-YU LI <s123600g@gmail.com>
2022-01-01 20:04:31 +0800

    add: Initialize project

---
+++ HttpClientFactoryDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientFactoryDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHttpClientFactory httpClientFactory = GetHttpClientFactory();
+            var baseAddress = new Uri("http://localhost:5030/");
+            var requestUrl = "WeatherForecast";
+
+            using (var client = httpClientFactory.CreateClient("SampleHttpClient"))
+            {
+                // 設定Time Out
+                client.Timeout = TimeSpan.FromSeconds(5);
+                client.BaseAddress = baseAddress;
+
+                try
+                {
+                    var response = await client.GetAsync(requestUrl);
+                    var contentStr = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(contentStr);
+                }
+                catch (TaskCanceledException ex)
+                {
+                    Console.WriteLine("TaskCanceledException");
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Call Second Api");
+
+                    var postContent = new StringContent("");
+
+                    var response2 = await client.PostAsync($"{requestUrl}?name=jyu", postContent);
+
+                    var contentStr2 = await response2.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(contentStr2);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine("Done!!");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 取得IHttpClientFactory 注入Service
+        /// </summary>
+        /// <returns></returns>
+        public static IHttpClientFactory GetHttpClientFactory()
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddHttpClient();
+
+            var serviceBuilder = serviceCollection.BuildServiceProvider();
+
+            var httpClientFactory = serviceBuilder.GetService<IHttpClientFactory>();
+
+            return httpClientFactory;
+        }
+    }
+}
---
+++ WebAPISample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPISample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            await Task.Delay(10000);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost(Name = "PostSecondApi")]
+        public ActionResult Post(
+            [FromQuery] string name
+        )
+        {
+            return Ok($"Hello {name} !!!");
+        }
+    }
+}
